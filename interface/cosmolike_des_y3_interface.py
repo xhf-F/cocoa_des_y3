92ecadacf2a4ccc97c1242e95e802a0ac47520c4
Vivian Miranda <vivianmiranda@arizona.edu>
2020-11-18 21:13:00 -0700

    update

--- interface/cosmolike_des_y3_interface.py
+++ interface/cosmolike_des_y3_interface.py
@@ -1,7 +1,7 @@
 def __bootstrap__():
    global __bootstrap__, __loader__, __file__
    import sys, pkg_resources, imp
-   __file__ = pkg_resources.resource_filename(__name__,'interface.so')
+   __file__ = pkg_resources.resource_filename(__name__,'cosmolike_des_y3_interface.so')
    __loader__ = None; del __bootstrap__, __loader__
    imp.load_dynamic(__name__,__file__)
 __bootstrap__()
