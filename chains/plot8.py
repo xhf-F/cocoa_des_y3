bf319e3029e1d688464002661c6826322a71cc9a
Vivian Miranda <vivianmiranda@arizona.edu>
2022-04-16 21:17:44 -0700

    update

--- chains/plot8.py
+++ chains/plot8.py
@@ -28,16 +28,16 @@ matplotlib.rcParams['savefig.bbox'] = 'tight'
 matplotlib.rcParams['savefig.format'] = 'pdf'
 
 # ADDING NEFF AS DERIVED PARAMETER
-parameter = [u'omegam',u'S8',u'ns',u'gamma']
+parameter = [u'omegam',u'S8',u'ns',u'H0']
 chaindir=r'.'
 
 analysissettings={'smooth_scale_1D':0.35,'smooth_scale_2D':0.35,'ignore_rows': u'0.35',
-'range_confidence' : u'0.025'}
+'range_confidence' : u'0.015'}
 
 analysissettings2={'smooth_scale_1D':0.35,'smooth_scale_2D':0.35,'ignore_rows': u'0.0',
-'range_confidence' : u'0.025'}
+'range_confidence' : u'0.015'}
 
-root_chains = ('EXAMPLE_MCMC14', 'EXAMPLE_MCMC16','EXAMPLE_MCMC10', 'EXAMPLE_MCMC15')
+root_chains = ('EXAMPLE_MCMC16','EXAMPLE_MCMC14','EXAMPLE_MCMC15','EXAMPLE_MCMC10')
 
 # --------------------------------------------------------------------------------
 samples=loadMCSamples('./' + root_chains[0],settings=analysissettings)
@@ -72,7 +72,7 @@ g.settings.lw_contour = 1.2
 g.settings.legend_rect_border = False
 g.settings.figure_legend_frame = False
 g.settings.axes_fontsize = 14.0
-g.settings.legend_fontsize = 12.75
+g.settings.legend_fontsize = 13.75
 g.settings.alpha_filled_add = 0.7
 g.settings.lab_fontsize=17
 g.legend_labels=False
@@ -80,19 +80,20 @@ g.legend_labels=False
 param_3d = None
 g.triangle_plot(['.VM_P8_TMP1', '.VM_P8_TMP2','.VM_P8_TMP3', '.VM_P8_TMP4'],parameter,
 plot_3d_with_param=param_3d,line_args=[
-{'lw': 1.0,'ls': 'solid', 'color':'royalblue'},
-{'lw': 1.0,'ls': 'solid', 'color':'firebrick'},
-{'lw': 2.0,'ls': '--', 'color':'royalblue'},
-{'lw': 2.0,'ls': '--', 'color':'firebrick'},
-{'lw': 2.5,'ls': '-.', 'color':'royalblue'},
-{'lw': 2.5,'ls': '-.', 'color':'firebrick'}],
-contour_colors=['royalblue','firebrick','royalblue','firebrick','royalblue','firebrick'],
-filled=True,shaded=False,
+{'lw': 1.2,'ls': 'solid', 'color':'cornflowerblue'},
+{'lw': 2.0,'ls': '-.', 'color':'firebrick'},
+{'lw': 2.0,'ls': '--', 'color': 'indigo'},
+{'lw': 1.2,'ls': 'solid', 'color': 'darkgoldenrod'}],
+contour_colors=['cornflowerblue','firebrick','indigo','darkgoldenrod'],
+contour_ls=['solid','-.','--','solid'], 
+contour_lws=[1.0,2.0,2.0,1.0],
+filled=[True,False,False,True],
+shaded=False,
 legend_labels=[
-'Planck low-$\\ell_{\\mathrm{EE}}$ + Planck high-$\\ell_{\\mathrm{TTTEEE}}$ (35 < $\\ell$ < 900) + DES-Y3', 
 'Planck low-$\\ell_{\\mathrm{EE}}$ + Planck high-$\\ell_{\\mathrm{TTTEEE}}$ (35 < $\\ell$ < 900)',
+'Planck low-$\\ell_{\\mathrm{EE}}$ + Planck high-$\\ell_{\\mathrm{TTTEEE}}$ (35 < $\\ell$ < 900) + DES-Y3', 
+'Planck low-$\\ell_{\\mathrm{EE}}$ + Planck high-$\\ell_{\\mathrm{TTTEEE}}$ (35 < $\\ell$ < 900) + SN (JLA)',
 'Planck low-$\\ell_{\\mathrm{EE}}$ + Planck high-$\\ell_{\\mathrm{TTTEEE}}$ (35 < $\\ell$ < 900) + SN (JLA) + DES-Y3', 
-'Planck low-$\\ell_{\\mathrm{EE}}$ + Planck high-$\\ell_{\\mathrm{TTTEEE}}$ (35 < $\\ell$ < 900) + SN (JLA)',],
-legend_loc=(0.3,0.83),
-imax_shaded=0)
+],
+legend_loc=(0.315,0.81))
 g.export()
