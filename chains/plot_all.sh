b62147340200f228b19e3c317b507ecb50bfb10c
Vivian Miranda <vivianmiranda@arizona.edu>
2022-05-12 09:20:14 -0700

    update

---
+++ chains/plot_all.sh
@@ -0,0 +1,12 @@
+$PYTHON3 plot0.py & 
+$PYTHON3 plot0v2.py & 
+$PYTHON3 plot0v3.py & 
+$PYTHON3 plot0v4.py &
+
+$PYTHON3 plot1.py &  
+$PYTHON3 plot1v2.py &  
+$PYTHON3 plot2.py &
+$PYTHON3 plot3.py 
+
+$PYTHON3 plot5.py &
+$PYTHON3 plot6.py 
